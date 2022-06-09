b9b00febead3a6236a2ea9ea8e057dc02afdf361
B3Funky <tyutin.s@list.ru>
2022-06-09 22:23:58 +0300

    feat: add navigation and working directory (nwd)

--- src/app.js
+++ src/app.js
@@ -2,21 +2,45 @@ import * as readline from 'node:readline';
 import * as os from 'node:os';
 import { stdin as input, stdout as output } from 'node:process';
 
+import { cd, up, ls } from './nwd/index.js';
 import { consoleMessages as message } from './utils/consoleMessages.js';
 
 const app = () => {
   const rl = readline.createInterface({ input, output });
 
-  process.chdir(os.homedir());
+  cd(os.homedir());
 
   output.write(message.welcome);
   output.write(message.currentlyDir(process.cwd()));
 
-  rl.on('line', (input) => {
-    output.write(message.currentlyDir(process.cwd()));
-
+  rl.on('line', async (input) => {
     if (input === '.exit') {
       rl.emit('SIGINT');
+    } else {
+      try {
+        const [operation, ...args] = input.split(' ');
+
+        switch (operation) {
+          case 'up':
+            up();
+            break;
+
+          case 'cd':
+            cd(args.join(' '));
+            break;
+
+          case 'ls':
+            output.write(message.ls(await ls(process.cwd())));
+            break;
+
+          default:
+            output.write(message.invalidInput);
+        }
+
+        output.write(message.currentlyDir(process.cwd()));
+      } catch (e) {
+        output.write(message.operationFailed);
+      }
     }
   });
 
--- src/utils/consoleMessages.js
+++ src/utils/consoleMessages.js
@@ -4,8 +4,20 @@ const userName = username();
 
 export const consoleMessages = {
   welcome: `\x1b[32m Welcome to the File Manager, ${userName}!\x1b[0m\n\n`,
+
   goodbye: `\x1b[36m\n Thank you for using File Manager, ${userName}!\x1b[0m\n`,
-  currentlyDir: (path) => `\x1b[33mYou are currently in ${path}\x1b[0m\n`,
+
   invalidInput: `\x1b[34mInvalid input!\x1b[0m\n`,
+
   operationFailed: `\x1b[31mOperation failed!\x1b[0m\n`,
+
+  currentlyDir: (path) => `\x1b[33mYou are currently in ${path}\x1b[0m\n`,
+
+  ls: (list) => {
+    let filesList = '\n\x1b[35m--- Files and directories: ---\n\n\x1b[32m';
+    list.forEach((fileName) => {
+      filesList += `${fileName}\n`;
+    });
+    return (filesList += '\x1b[0m\n');
+  },
 };
